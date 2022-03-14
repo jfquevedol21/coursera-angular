55df48d30e87e97264b399d3af741321155583c8
Ferusho <jferntarly@gmail.com>
2022-03-14 15:23:44 -0500

    single page application

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,7 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule
       
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
